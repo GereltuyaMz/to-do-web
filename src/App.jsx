96c3d0d0a976a1a1df00bc493b811fe5642e6f3c
GereltuyaMz <gegiimz96@gmail.com>
2025-05-08 17:16:21 +0800

    create multi select dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Button from "./components/Button";
 import Task from "./components/Task";
 import tasks from "./data/todo";
+import { MultiSelectDropdown } from "./components/MultiSelectDropdown";
+import UserForm from "./components/UserForm";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -77,87 +79,88 @@ function App() {
   };
 
   return (
-    <div className="flex justify-center bg-[#F3F4F6] h-screen w-scree">
-      <div className="w-[377px] size-fit bg-white mt-[60px] shadow-md rounded-md py-[24px] px-[16px] flex flex-col items-center">
-        <div className="flex flex-col w-full gap-5 mb-5">
-          <h1 className="text-xl font-semibold text-center">To-Do list</h1>
-          <div className="flex gap-[6px]">
-            <input
-              onChange={(event) => {
-                setInputValue(event.target.value);
-              }}
-              value={inputValue}
-              className="h-[40px] border w-full border-[#E4E4E7] rounded-md pl-4"
-              type="text"
-              placeholder="Add a new task..."
-            />
-            <Button handleClick={createTask} text="Add" />
-          </div>
-        </div>
+    // <div className="flex justify-center bg-[#F3F4F6] h-screen w-scree">
+    //   <div className="w-[377px] size-fit bg-white mt-[60px] shadow-md rounded-md py-[24px] px-[16px] flex flex-col items-center">
+    //     <div className="flex flex-col w-full gap-5 mb-5">
+    //       <h1 className="text-xl font-semibold text-center">To-Do list</h1>
+    //       <div className="flex gap-[6px]">
+    //         <input
+    //           onChange={(event) => {
+    //             setInputValue(event.target.value);
+    //           }}
+    //           value={inputValue}
+    //           className="h-[40px] border w-full border-[#E4E4E7] rounded-md pl-4"
+    //           type="text"
+    //           placeholder="Add a new task..."
+    //         />
+    //         <Button handleClick={createTask} text="Add" />
+    //       </div>
+    //     </div>
 
-        <div className="flex gap-[6px] mb-5">
-          <Button
-            isActive={filterStatus === "all"}
-            isSmall={true}
-            text="All"
-            handleClick={clearFilter}
-          />
-          <Button
-            isActive={filterStatus === "active"}
-            isSmall={true}
-            text="Active"
-            handleClick={filterActive}
-          />
-          <Button
-            isActive={filterStatus === "completed"}
-            isSmall={true}
-            text="Completed"
-            handleClick={filterCompleted}
-          />
-        </div>
+    //     <div className="flex gap-[6px] mb-5">
+    //       <Button
+    //         isActive={filterStatus === "all"}
+    //         isSmall={true}
+    //         text="All"
+    //         handleClick={clearFilter}
+    //       />
+    //       <Button
+    //         isActive={filterStatus === "active"}
+    //         isSmall={true}
+    //         text="Active"
+    //         handleClick={filterActive}
+    //       />
+    //       <Button
+    //         isActive={filterStatus === "completed"}
+    //         isSmall={true}
+    //         text="Completed"
+    //         handleClick={filterCompleted}
+    //       />
+    //     </div>
 
-        {/* tasks */}
-        {/* tasks */}
-        <div className="w-full flex flex-col gap-5">
-          {filteredData.map((task) => {
-            return (
-              <Task
-                key={task.id}
-                id={task.id}
-                text={task.text}
-                status={task.status}
-                deleteTask={deleteTask}
-                toggleStatus={toggleStatus}
-              />
-            );
-          })}
-        </div>
+    //     {/* tasks */}
+    //     {/* tasks */}
+    //     <div className="w-full flex flex-col gap-5">
+    //       {filteredData.map((task) => {
+    //         return (
+    //           <Task
+    //             key={task.id}
+    //             id={task.id}
+    //             text={task.text}
+    //             status={task.status}
+    //             deleteTask={deleteTask}
+    //             toggleStatus={toggleStatus}
+    //           />
+    //         );
+    //       })}
+    //     </div>
 
-        {filteredData.length === 0 && (
-          <p className="text-center text-[#6B7280] text-sm my-5">
-            No tasks yet. Add one above!
-          </p>
-        )}
+    //     {filteredData.length === 0 && (
+    //       <p className="text-center text-[#6B7280] text-sm my-5">
+    //         No tasks yet. Add one above!
+    //       </p>
+    //     )}
 
-        <div className="border-t border-[#E4E4E7] w-full my-5"></div>
+    //     <div className="border-t border-[#E4E4E7] w-full my-5"></div>
 
-        {/* task bar */}
-        <div className="pb-1 flex justify-between text-sm w-full">
-          <p className="text-[#6B7280]">
-            {completedNumber} of {data.length} tasks completed
-          </p>
-          <button onClick={clearCompleted} className="text-[#F44444]">
-            Clear completed
-          </button>
-        </div>
-        <p className="mt-10 text-xs text-[#6B7280]">
-          Powered by
-          <a href="www.pinecone.mn" className="text-[#3B73ED] ml-1">
-            Pinecone academy
-          </a>
-        </p>
-      </div>
-    </div>
+    //     {/* task bar */}
+    //     <div className="pb-1 flex justify-between text-sm w-full">
+    //       <p className="text-[#6B7280]">
+    //         {completedNumber} of {data.length} tasks completed
+    //       </p>
+    //       <button onClick={clearCompleted} className="text-[#F44444]">
+    //         Clear completed
+    //       </button>
+    //     </div>
+    //     <p className="mt-10 text-xs text-[#6B7280]">
+    //       Powered by
+    //       <a href="www.pinecone.mn" className="text-[#3B73ED] ml-1">
+    //         Pinecone academy
+    //       </a>
+    //     </p>
+    //   </div>
+    // </div>
+    <MultiSelectDropdown />
   );
 }
 
